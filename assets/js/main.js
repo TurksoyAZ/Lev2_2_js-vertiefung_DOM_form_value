cbcdf967f4bfdce0a97d62729070922bcbf990be
TurksoyAz <supercoder@AirvonSperCoder.fritz.box>
2022-09-27 16:57:52 +0200

    Lev2_2_js-vertiefung_DOM_form_value

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+
+
+
+
+const vorName=document.getElementById("vorname");
+const nachName=document.getElementById("nachname");
+const land=document.getElementById("land");
+
+
+function sendInfo(){
+    console.log("Full Name: "+vorName.value,nachName.value,"Land: "+land.value);
+}
+
+
+
+
+
